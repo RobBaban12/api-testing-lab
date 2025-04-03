eae1c4a15e623d8281ca7191e18ba07c668ed02d
Prince Herrera <princeherreracpu@gmail.com>
2025-04-03 21:36:35 +0800

    updated files

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,23 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 
-if (process.env.NODE_ENV === "test") {
-  dotenv.config({ path: ".env.test" });
-} else {
-  dotenv.config();
-}
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url:
-        process.env.NODE_ENV === "test"
-          ? process.env.DATABASE_URL
-          : process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 let supabase = null;
 if (process.env.SUPABASE_URL && process.env.SUPABASE_KEY) {
